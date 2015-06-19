435daf8e22da5a2cce788c4bb4c69900a6b20b9b
Łukasz Adamczak <lukasz@czak.pl>
2015-06-19 23:14:01 +0200

    Simple structure with MainWindowController

---
+++ Trianglify/MainWindowController.swift
@@ -0,0 +1,23 @@
+//
+//  MainWindowController.swift
+//  Trianglify
+//
+//  Created by Łukasz Adamczak on 19.06.2015.
+//  Copyright (c) 2015 Łukasz Adamczak. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override var windowNibName: String {
+        return "MainWindowController"
+    }
+    
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
