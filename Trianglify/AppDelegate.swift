b8fbfa265c87a33ea8291e9e8945c6e57a66459b
Łukasz Adamczak <lukasz@czak.pl>
2015-06-19 23:07:24 +0200

    Initial Commit

---
+++ Trianglify/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Trianglify
+//
+//  Created by Łukasz Adamczak on 19.06.2015.
+//  Copyright (c) 2015 Łukasz Adamczak. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
